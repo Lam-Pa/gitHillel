0c6083662013c46f89798bc929919f79be4dd928
Lam-Pa <reyvishnyakov@gmail.com>
2020-07-30 21:15:40 +0300

    on branch dev

--- hello_world/src/com/company/Main.java
+++ hello_world/src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Second commit for git");
+        System.out.println("Commit and checkout to anouther branch for git");
 
     }
 }
